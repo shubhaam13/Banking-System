3498dc7422b4073f43c81965ce6401e7ec43e5df
SHUBHAM KUMAR <imsky004@gmail.com>
2021-06-12 19:27:31 +0530

    update

--- createuser.php
+++ createuser.php
@@ -1,75 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create User</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/table.css">
-    <link rel="stylesheet" type="text/css" href="css/navbar.css">
-    <link rel="stylesheet" type="text/css" href="css/createuser.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Create User</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/table.css">
+<link rel="stylesheet" type="text/css" href="css/navbar.css?v=<?php echo time(); ?>">
+<link rel="stylesheet" type="text/css" href="css/createuser.css?v=<?php echo time(); ?>">
+<link rel="stylesheet" type="text/css" href="css/style.css?v=<?php echo time(); ?>">
 </head>
 
-<body style="background-color : #82E0AA;">
+<body>
 <?php
-    include 'config.php';
-    if(isset($_POST['submit'])){
-    $name=$_POST['name'];
-    $email=$_POST['email'];
-    $balance=$_POST['balance'];
-    $sql="insert into users(name,email,balance) values('{$name}','{$email}','{$balance}')";
-    $result=mysqli_query($conn,$sql);
-    if($result){
-               echo "<script> alert('Hurray! User created');
-                               window.location='transfermoney.php';
-                     </script>";
-                    
-    }
-  }
+include 'config.php';
+if(isset($_POST['submit'])){
+$name=$_POST['name'];
+$email=$_POST['email'];
+$balance=$_POST['balance'];
+$sql="insert into users(name,email,balance) values('{$name}','{$email}','{$balance}')";
+$result=mysqli_query($conn,$sql);
+if($result){
+echo "<script> alert('User created Sucessfully!');
+window.location='transfermoney.php';
+</script>";
+
+}
+}
 ?>
 
 <?php
-  include 'navbar.php';
+include 'navbar.php';
 ?>
 
-        <h2 class="text-center pt-4" style="color : black;">Create a New Bank User</h2>
-        <br>
+<div class="maincontainer" ">
 
-  <div class="background">
-  <div class="container">
-    <div class="screen">
-      <div class="screen-header">
-        <div class="screen-header-right">
-          <div class="screen-header-ellipsis"></div>
-          <div class="screen-header-ellipsis"></div>
-          <div class="screen-header-ellipsis"></div>
-        </div>
-      </div>
-      <div class="screen-body">
-        <div class="screen-body-item left">
-          <img class="img-fluid" src="img/user3.jpg" style="border: none; border-radius: 50%;">
-        </div>
-        <div class="screen-body-item">
-          <form class="app-form" method="post">
-            <div class="app-form-group">
-              <input class="app-form-control" placeholder="NAME" type="text" name="name" required>
-            </div>
-            <div class="app-form-group">
-              <input class="app-form-control" placeholder="EMAIL" type="email" name="email" required>
-            </div>
-            <div class="app-form-group">
-              <input class="app-form-control" placeholder="BALANCE" type="number" name="balance" required>
-            </div>
-            <br>
-            <div class="app-form-group button">
-              <input class="app-form-button" type="submit" value="CREATE" name="submit"></input>
-              <input class="app-form-button" type="reset" value="RESET" name="reset"></input>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
+<h2 class="text-center pt-4"></h2>
+<br>
+
+<div class="background">
+<div class="container">
+<div class="screen">
+<div class="screen-header">
+<div class="screen-header-right">
+<div class="screen-header-ellipsis"></div>
+<div class="screen-header-ellipsis"></div>
+<div class="screen-header-ellipsis"></div>
+</div>
+</div>
+<div class="screen-body">
+<div class="screen-body-item left">
+<img class="img-fluid" src="img/user3.jpg" style="border: none; border-radius: 50%;">
+</div>
+<div class="screen-body-item">
+<form class="app-form" method="post">
+<div class="app-form-group">
+<input class="app-form-control" placeholder="NAME" type="text" name="name" required>
+</div>
+<div class="app-form-group">
+<input class="app-form-control" placeholder="EMAIL" type="email" name="email" required>
+</div>
+<div class="app-form-group">
+<input class="app-form-control" placeholder="BALANCE" type="number" name="balance" required>
+</div>
+<br>
+<div class="app-form-group button">
+<input class="app-form-button" type="submit" value="CREATE" name="submit"></input>
+<input class="app-form-button" type="reset" value="RESET" name="reset"></input>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
 </div>
 <footer class="text-center mt-5 py-2">
 <p>Made with ❤️ by <b>SHUBHAM KUMAR</b></p>
